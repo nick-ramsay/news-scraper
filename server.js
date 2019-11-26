541022aed85fa5b6f20447e5461ea7129f637046
Nick Ramsay <NickRamsay1989@gmail.com>
2019-11-26 23:33:38 +1100

    Scraper logic now functional

--- server.js
+++ server.js
@@ -14,9 +14,41 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.mongolab - rectangular - 08989 || "mongodb://localhost/news-scraper";
+var MONGODB_URI = /*process.env.mongolab - rectangular - 08989 || */"mongodb://localhost/news-scraper";
 
 mongoose.connect(MONGODB_URI);
+
+app.get("/scrape", function(req,res) {
+    axios.get("https://news.google.com/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen").then(function(response){
+        var $ = cheerio.load(response.data);
+        //console.log(response.data);
+
+        $("article h3").each(function(i,element) {
+            var result = {};
+
+            result.title = $(this)
+            .text();
+
+            result.link = "news.google.com" + $(this)
+            .children("a")
+            .attr("href");
+
+            result.description = $(this)
+            .next()
+            .children("span")
+            .text();
+
+            result.photo_url = $(this)
+            .parents()
+            .parent()
+            .children("a")
+            .children("figure")
+            .children("img")
+            .attr("src");
+            console.log(result);
+        })
+    });
+});
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
