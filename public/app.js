9ae8c3c7b772d03cfa683382448ddabd440b6918
Nick Ramsay <NickRamsay1989@gmail.com>
2019-11-28 23:32:27 +1100

    News data now rendering with proper format

--- public/app.js
+++ public/app.js
@@ -1,18 +1,21 @@
 $.getJSON("/articles", function (data) {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
-        var card = '<div class="card mb-3" style="max-width: 540px;">';
-        var cardRow = '<div class="row no-gutters">';
-        var cardBody = '<div class="col-md-8"><div class="card-body">';
-        var cardHeading = '<h5 class="card-title"><a data-id="' + data[i]._id + '" href="' + data[i].url + '">' + data[i].headline + '</a></h5>';
-        var cardSummary = '<p class="card-text">' + data[i].summary + '</p>';
-        var cardImage = '<div class="col-md-4"><img src="' + data[i].photo_url + '" class="card-img" alt="' + data[i].headline + '"></div>';
+        var card = '<div class="card"></div>';
+        var cardRow = '<div class="row">';
+        var cardText = '<div class="col-md-8"><div class="card-body"><h5 class="card-title"><a data-id="' + data[i]._id + '" href="' + data[i].url + '">' + data[i].headline + '</a></h5><p class="card-text">' + data[i].summary + '</p></div></div>';
+        var cardSaveButton = '<div class="col-md-1" style="margin: auto; text-align: center;"><button class="btn btn-danger"><img src="./images/saveArticleIcon.png" style="max-height:24px; max-width: 24px;"></button></div>';
+        var cardImage = '<div class="col-md-3" style="text-align:center;vertical-align:center;"><img src="' + data[i].photo_url + '" class="card-img m-1" alt="' + data[i].headline + '" style="max-height: 150px; max-width:150px;"></div>';
+        cardRow += cardText;
+        cardRow += cardSaveButton;
+        switch (data[i].photo_url) {
+            case undefined:
+                break;
+            default:
+                cardRow += cardImage;
+        }
 
         card += cardRow;
-        card += cardBody;
-        card += cardHeading;
-        card += cardSummary;
-        card += cardImage;
 
         $("#articles").append(card);
         //$("#articles").append("<p><a data-id='" + data[i]._id + "' href=" + data[i].url + ">" + data[i].headline + "</a></p>");
