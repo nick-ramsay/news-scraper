19c0e03d2fd58b758f93ab7e10c9d1c4c5113b1d
Nick Ramsay <48420063+nick-ramsay@users.noreply.github.com>
2019-11-28 14:36:44 +1100

    Completed DOM code for appending news to front end

--- public/app.js
+++ public/app.js
@@ -1,10 +1,24 @@
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
-      $("#articles").append("<p><a data-id='" + data[i]._id + "' href=" + data[i].url + ">" + data[i].headline + "</a></p>");
+        var card = '<div class="card mb-3" style="max-width: 540px;">';
+        var cardRow = '<div class="row no-gutters">';
+        var cardBody = '<div class="col-md-8"><div class="card-body">';
+        var cardHeading = '<h5 class="card-title"><a data-id="' + data[i]._id + '" href="' + data[i].url + '">' + data[i].headline + '</a></h5>';
+        var cardSummary = '<p class="card-text">' + data[i].summary + '</p>';
+        var cardImage = '<div class="col-md-4"><img src="' + data[i].photo_url + '" class="card-img" alt="' + data[i].headline + '"></div>';
+
+        card += cardRow;
+        card += cardBody;
+        card += cardHeading;
+        card += cardSummary;
+        card += cardImage;
+
+        $("#articles").append(card);
+        //$("#articles").append("<p><a data-id='" + data[i]._id + "' href=" + data[i].url + ">" + data[i].headline + "</a></p>");
     }
-  });
-  
+});
+
 
 $(document).on("click", "#scrapeBtn", function () {
     $.ajax({
@@ -23,4 +37,3 @@ $(document).on("click", "#clearArticleBtn", function () {
         console.log("Scraped articles dropped!")
     )
 });
-
